3730fa136fb9728116440623d893c0d400ad7241
amanda093 <vovigadagamer@gmail.com>
2024-08-02 16:12:57 -0300

    adicionei banco de dados

--- app/src/main/java/com/example/appdatabase/MainActivity.kt
+++ app/src/main/java/com/example/appdatabase/MainActivity.kt
@@ -5,17 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,8 +23,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontVariation.weight
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,6 +55,8 @@ fun AppPreview() {
 }
 @Composable
 fun App() {
+    var nome by remember { mutableStateOf("") }
+    var telefone by remember { mutableStateOf("") }
     Column(
         Modifier
             .background(Color.White)
@@ -71,35 +70,33 @@ fun App() {
         Row {
             Text(
                 text = "App Database",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
                 fontSize = 20.sp,
                 color = Color.Black)
         }
         Row {
-            TextField(TextFieldName = "Nome")
+            OutlinedTextField(
+                value = nome,
+                onValueChange = { nome = it },
+                label = { Text("Nome ")}
+            )
         }
         Row {
-            TextField(TextFieldName = "Idade")
+            OutlinedTextField(
+                value = telefone,
+                onValueChange = { telefone = it },
+                label = { Text("Telefone ")}
+            )
         }
         Row(Modifier.padding(20.dp)) {
         }
         Row {
-            Button(
-                onClick = { /*TODO*/ }
+            Button(onClick = { /*TODO*/ }
             ) {
                 Text(text = "Cadastrar")
             }
         }
     }
 }
-@Composable
-fun TextField(TextFieldName: String) {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    OutlinedTextField(
-        value = text,
-        onValueChange = {
-            text = it
-        },
-        label = { Text(text = TextFieldName) },
-        placeholder = { Text(text = "") },
-    )
-}
+
